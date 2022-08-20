7f871411f3f226b4a1dffe9427aff7f4c65e7322
Anujesh Ansh <anshanujesh@gmail.com>
2022-08-20 09:56:36 +0530

    with 2 dices

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -21,22 +22,53 @@ class MainActivity : AppCompatActivity() {
             //val resultTextView: TextView = findViewById(R.id.textView) // creates a variable to store the output value
             //resultTextView.text = "6" // to store the value in form of string as it only prints string
 
-
         }
+
+        rollDice() // to show a random number in the start, without clicking the button, iske bina app khaali khaali lagta hai
     }
 
     private fun rollDice() {
         val dice1 = Dice(6) // calls and send a value to class Dice, i.e creates object for class Dice
         val dice2 = Dice(6)
+
         val dice1Roll = dice1.roll() // call the function and stores the return value
         val dice2Roll = dice2.roll()
 
 
-        val resultTextView1: TextView = findViewById(R.id.textView)
-        resultTextView1.text = (dice1Roll).toString() // toString - to convert the following into string; It prints the value stored in diceRoll, after changing it into string using .toString
+        // to display the number for dice 1
+        //val resultTextView1: TextView = findViewById(R.id.textView)
+        //resultTextView1.text = (dice1Roll).toString() // toString - to convert the following into string; It prints the value stored in diceRoll, after changing it into string using .toString
 
-        val resultTextView2: TextView = findViewById(R.id.textView5)
-        resultTextView2.text = (dice2Roll).toString()
+        // to display the number for dice 2
+        //val resultTextView2: TextView = findViewById(R.id.textView5)
+        //resultTextView2.text = (dice2Roll).toString()
+
+        val dice1Image: ImageView=findViewById(R.id.imageView)
+
+        val drawable1Resource=when(dice1Roll){  // statement to check the number, R.drawable can change the content of diceImage, and place the required image to be displayed
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {R.drawable.dice_6} // use of else is there, to save the 'when' function from being exhausted
+        }
+        dice1Image.setImageResource(drawable1Resource)  // set.ImageResource - it changes the content of the image view
+
+        //diceImage.contentDescription = dice1Roll.toString() // to read out the number loud, but sunai nahi de raha hai
+
+        val dice2Image: ImageView=findViewById(R.id.imageView2)
+
+        val drawable2Resource=when(dice2Roll){  // statement to check the number, R.drawable can change the content of diceImage, and place the required image to be displayed
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {R.drawable.dice_6} // use of else is there, to save the 'when' function from being exhausted
+        }
+        dice2Image.setImageResource(drawable2Resource)
+
     }
 }
 
