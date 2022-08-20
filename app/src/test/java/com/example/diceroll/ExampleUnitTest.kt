9850342d38aac4cc03302a03edab63f6d860f1cf
Anujesh Ansh <anshanujesh@gmail.com>
2022-08-20 12:04:16 +0530

    with 1 dice with content description & test too

--- app/src/test/java/com/example/diceroll/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroll/ExampleUnitTest.kt
@@ -2,16 +2,29 @@ package com.example.diceroll
 
 import org.junit.Test
 
-import org.junit.Assert.*
+//import org.junit.Assert.*
+import org.junit.Assert.assertTrue
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+/*class ExampleUnitTest {
+    @Test // used to tell the system that it is a test function
+    fun addition_isCorrect() { // functions are true to their name, jaise isme addition check hota hai
+        assertEquals(5, 2 + 3) //aur yeh equals to dekhta hai
+    }
+}*/
+
+class ExampleUnitTest{
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generate_number(){
+        val side = 6
+        val dice=Dice(side)
+        val rollResult=dice.roll()
+        // val rollResult=7 // just for testing
+        assertTrue("The value of rollResult was not between 1 and 6",rollResult in 1..side)
     }
 }
